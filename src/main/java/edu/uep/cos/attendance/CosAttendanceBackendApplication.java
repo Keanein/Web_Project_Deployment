3a8eac157bd52c9d785f03a831bdedba83f4ba6d
Keanu <alabadokeanu90@gmail.com>
2025-12-27 02:33:58 +0800

    Configure entity scan and JPA repositories

--- src/main/java/edu/uep/cos/attendance/CosAttendanceBackendApplication.java
+++ src/main/java/edu/uep/cos/attendance/CosAttendanceBackendApplication.java
@@ -4,9 +4,14 @@ package edu.uep.cos.attendance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 // Main entry point of the COS Attendance Spring Boot application
 @SpringBootApplication
+@EntityScan("edu.uep.cos.attendance.entity")
+@EnableJpaRepositories("edu.uep.cos.attendance.repository")
 public class CosAttendanceBackendApplication {
 
     // Starts and runs the Spring Boot application
